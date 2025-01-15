f81e61f5015de449cb9cae73844fd211a10bdc89
Firzus <92395888+Firzus@users.noreply.github.com>
2025-01-15 10:24:29 +0100

    Update PlayerCondition.h

--- LightEngine/PlayerCondition.h
+++ LightEngine/PlayerCondition.h
@@ -14,3 +14,39 @@ class PlayerCondition_OnMatchEnd : public Condition<Player>
 public:
 	bool OnTest(Player* owner) override;
 };
+
+class PlayerCondition_OnAdversaryHasBall: public Condition<Player>
+{
+public:
+	bool OnTest(Player* owner) override;
+};
+
+class PlayerCondition_OnAllyHasBall : public Condition<Player>
+{
+public:
+	bool OnTest(Player* owner) override;
+};
+
+class PlayerCondition_OnCatchBallByAlly : public Condition<Player>
+{
+public:
+	bool OnTest(Player* owner) override;
+};
+
+class PlayerCondition_OnCatchBallByAdversary : public Condition<Player>
+{
+public:
+	bool OnTest(Player* owner) override;
+};
+
+class PlayerCondition_OnPassedBallToAlly : public Condition<Player>
+{
+public:
+	bool OnTest(Player* owner) override;
+};
+
+class PlayerCondition_OnLooseBallFromAdversary : public Condition<Player>
+{
+public:
+	bool OnTest(Player* owner) override;
+};
